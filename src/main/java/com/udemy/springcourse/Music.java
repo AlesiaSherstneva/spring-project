bd0f8831911cc94757b2c9781f8fe230fc87efd3
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-10 21:04:30 +0300

    Lesson 5 - IoC

---
+++ src/main/java/com/udemy/springcourse/Music.java
@@ -0,0 +1,5 @@
+package com.udemy.springcourse;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/com/udemy/springcourse/RockMusic.java
@@ -0,0 +1,8 @@
+package com.udemy.springcourse;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Billy Talent - Fallen leaves";
+    }
+}
