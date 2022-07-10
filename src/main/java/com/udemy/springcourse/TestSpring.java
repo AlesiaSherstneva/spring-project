f50a841f42cddb736d2c7f1c3a48a42cdf69b13a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-10 21:04:56 +0300

    Lesson 5 - IoC

--- src/main/java/com/udemy/springcourse/TestSpring.java
+++ src/main/java/com/udemy/springcourse/TestSpring.java
@@ -6,8 +6,12 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer player = new MusicPlayer(music);
+
+        player.playMusic();
+
         context.close();
     }
 }
