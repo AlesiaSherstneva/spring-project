99c376f8b421099505ae706cccd78da0806590ff
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-10 23:18:06 +0300

    Lesson 6 - Dependency Injection

--- src/main/java/com/udemy/springcourse/TestSpring.java
+++ src/main/java/com/udemy/springcourse/TestSpring.java
@@ -6,9 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
 
         player.playMusic();
 
