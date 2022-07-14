dbe13c7e4ddfc13aefc0413df4f80709ddba45bb
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-15 00:44:26 +0300

    Lesson 12 - Homework

--- src/main/java/com/udemy/springcourse/MusicPlayer.java
+++ src/main/java/com/udemy/springcourse/MusicPlayer.java
@@ -1,7 +1,6 @@
 package com.udemy.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
