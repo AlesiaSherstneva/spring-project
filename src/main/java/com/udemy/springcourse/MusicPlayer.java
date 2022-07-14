2e6473990b269c6ab94fba88c62ffe3b099145b4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-15 00:48:05 +0300

    Lesson 12 - Homework

--- src/main/java/com/udemy/springcourse/MusicPlayer.java
+++ src/main/java/com/udemy/springcourse/MusicPlayer.java
@@ -1,24 +1,26 @@
 package com.udemy.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private final ClassicalMusic classicalMusic;
-    private final RockMusic rockMusic;
+    private final Music music1;
+    private final Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public void playMusic(Genre genre) {
         if(genre == Genre.CLASSICAL) {
-            System.out.println(classicalMusic.getSong());
+            System.out.println(music1.getSong());
         } else {
-            System.out.println(rockMusic.getSong());
+            System.out.println(music2.getSong());
         }
     }
 }
