87d2f79503b4b8f784e06cdeadef2b8704d5b143
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-20 01:18:58 +0300

    Lesson 17 - Spring MVC (Java-configuration)

---
+++ src/main/java/com/udemy/springcourse/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package com.udemy.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
