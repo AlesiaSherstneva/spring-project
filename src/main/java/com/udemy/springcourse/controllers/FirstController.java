9b60b60c37f58b007da3053b582beb0e086202ed
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-21 23:24:31 +0300

    Lesson 20 - @RequestParam

--- src/main/java/com/udemy/springcourse/controllers/FirstController.java
+++ src/main/java/com/udemy/springcourse/controllers/FirstController.java
@@ -3,20 +3,16 @@ package com.udemy.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
         System.out.println("Hello, " + name + " " + surname);
-
         return "first/hello";
     }
 
