a189a0430cd03709852db18e378065144e87c48c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-21 23:12:52 +0300

    Lesson 20 - HttpServletRequest

--- src/main/java/com/udemy/springcourse/controllers/FirstController.java
+++ src/main/java/com/udemy/springcourse/controllers/FirstController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
