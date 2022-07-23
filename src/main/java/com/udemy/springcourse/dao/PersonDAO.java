5daddcda6503a2019c3d368e80e77bd3570a4b3a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-23 18:18:01 +0300

    Lesson 22 - CRUD, Read operation

---
+++ src/main/java/com/udemy/springcourse/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package com.udemy.springcourse.dao;
+
+import com.udemy.springcourse.pojo.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
