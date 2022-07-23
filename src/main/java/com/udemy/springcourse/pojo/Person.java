94115a6a41c1f73930858e3bff723912a7c5e793
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-23 19:19:36 +0300

    Lesson 23 - CRUD, Create operation

--- src/main/java/com/udemy/springcourse/pojo/Person.java
+++ src/main/java/com/udemy/springcourse/pojo/Person.java
@@ -4,6 +4,9 @@ public class Person {
     private int id;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
