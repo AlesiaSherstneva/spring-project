3266e82acd18e6e482dfd0439de5118aaa50774e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-05 15:14:12 +0300

    Save PersonMapper (just in case)

---
+++ src/main/java/com/udemy/springcourse/dao/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.udemy.springcourse.dao;
+
+import com.udemy.springcourse.pojo.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Person person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setAge(resultSet.getInt("age"));
+        person.setEmail(resultSet.getString("email"));
+        return person;
+    }
+}
