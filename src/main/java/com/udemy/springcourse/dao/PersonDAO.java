717e47234fc728b600f4e3a73d906b985e29b393
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-06 22:31:56 +0300

    Lesson 31 - Autoincrement

--- src/main/java/com/udemy/springcourse/dao/PersonDAO.java
+++ src/main/java/com/udemy/springcourse/dao/PersonDAO.java
@@ -42,7 +42,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES (1, ?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES (?, ?, ?)",
                 person.getName(), person.getAge(), person.getEmail());
     }
 
