f357082075c64bc1b5413208808232140736b6af
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-19 17:45:44 +0300

    Add person DAO

---
+++ project1/src/main/java/com/udemy/springcourse/dao/PersonDAO.java
@@ -0,0 +1,23 @@
+package com.udemy.springcourse.dao;
+
+import com.udemy.springcourse.pojo.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> showPeople() {
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
