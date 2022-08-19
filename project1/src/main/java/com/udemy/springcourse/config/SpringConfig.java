930ac54c09dfe135cf25b4b98782ec685214e889
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-19 17:13:10 +0300

    Edit spring configuration to resolve Russian

--- project1/src/main/java/com/udemy/springcourse/config/SpringConfig.java
+++ project1/src/main/java/com/udemy/springcourse/config/SpringConfig.java
@@ -54,6 +54,7 @@ public class SpringConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
         registry.viewResolver(resolver);
     }
 
--- project1/src/main/java/com/udemy/springcourse/config/SpringMvcDispatcherServletInitializer.java
+++ project1/src/main/java/com/udemy/springcourse/config/SpringMvcDispatcherServletInitializer.java
@@ -1,7 +1,15 @@
 package com.udemy.springcourse.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
+
+@SuppressWarnings("NullableProblems")
 public class SpringMvcDispatcherServletInitializer
         extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -11,12 +19,28 @@ public class SpringMvcDispatcherServletInitializer
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringConfig.class};
+        return new Class[]{SpringConfig.class};
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerCharacterEncodingFilter(aServletContext);
+    }
+
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
     }
 }
