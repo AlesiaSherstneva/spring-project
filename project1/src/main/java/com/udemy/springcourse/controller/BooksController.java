c3b3647cd3b684c295977c25dfcf6b96f0f1b6ee
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-23 00:37:07 +0300

    Add BooksController

---
+++ project1/src/main/java/com/udemy/springcourse/controller/BooksController.java
@@ -0,0 +1,25 @@
+package com.udemy.springcourse.controller;
+
+import com.udemy.springcourse.dao.BookDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BooksController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping
+    public String showBooks(Model model) {
+        model.addAttribute("books", bookDAO.showBooks());
+        return "books/show";
+    }
+}
