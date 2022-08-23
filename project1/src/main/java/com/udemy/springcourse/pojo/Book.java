aed25c62988db5a6b93289e3a9a1b5a0895ad259
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-23 18:19:55 +0300

    Add validators

--- project1/src/main/java/com/udemy/springcourse/pojo/Book.java
+++ project1/src/main/java/com/udemy/springcourse/pojo/Book.java
@@ -1,10 +1,27 @@
 package com.udemy.springcourse.pojo;
 
+import com.udemy.springcourse.validators.CurrentYear;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class Book {
     private int id;
     private Integer person_id;
+
+    @NotEmpty(message = "Это поле не может быть пустым")
+    @Pattern(regexp = "[А-ЯЁ][а-яА-ЯёЁ\\-\\s]+",
+            message = "Должен быть формат \"Название\" на русском языке")
     private String title;
+
+    @NotEmpty(message = "Это поле не может быть пустым")
+    @Pattern(regexp = "[А-ЯЁ][а-яА-ЯёЁ\\-]+\\s[А-ЯЁ][а-яА-ЯёЁ\\-]+",
+            message = "Должен быть формат \"Фамилия Имя\" на русском языке")
     private String author;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше 1445")
+    @CurrentYear
     private int year;
 
     public Book() {
