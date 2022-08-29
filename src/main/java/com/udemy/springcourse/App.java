1688f30f264d677362015f2d42f9979ed8a8914c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-08-29 22:39:56 +0300

    Lesson 49 - Saving entities

--- src/main/java/com/udemy/springcourse/App.java
+++ src/main/java/com/udemy/springcourse/App.java
@@ -13,9 +13,14 @@ public class App {
         try (sessionFactory) {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
-            Person person = session.get(Person.class, 1);
 
-            System.out.println(person.getName() + ", " + person.getAge());
+            Person person1 = new Person("Test1", 30);
+            Person person2 = new Person("Test2", 40);
+            Person person3 = new Person("Test3", 50);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
 
             session.getTransaction().commit();
         }
--- src/main/java/com/udemy/springcourse/pojo/Person.java
+++ src/main/java/com/udemy/springcourse/pojo/Person.java
@@ -1,9 +1,6 @@
 package com.udemy.springcourse.pojo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Person")
@@ -11,6 +8,7 @@ public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
@@ -23,18 +21,13 @@ public class Person {
     public Person() {
     }
 
-    public Person(int id, String name, int age) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
