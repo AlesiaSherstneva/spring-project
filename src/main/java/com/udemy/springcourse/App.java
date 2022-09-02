51a0963ef9732966a80a7a30b132b5a2a6d62290
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-09-02 22:46:44 +0300

    Lesson 50 - Updating in Hibernate

--- src/main/java/com/udemy/springcourse/App.java
+++ src/main/java/com/udemy/springcourse/App.java
@@ -9,18 +9,14 @@ public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
 
         try (sessionFactory) {
-            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Person person1 = new Person("Test1", 30);
-            Person person2 = new Person("Test2", 40);
-            Person person3 = new Person("Test3", 50);
-
-            session.save(person1);
-            session.save(person2);
-            session.save(person3);
+            Person person = session.get(Person.class, 2);
+            person.setName("New name");
 
             session.getTransaction().commit();
         }
