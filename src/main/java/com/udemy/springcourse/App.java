a30a9400fa6b34b821146931c1c4a0f50d13fa9a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-09-04 13:47:39 +0300

    Lesson 51 - Create query using LIKE pattern

--- src/main/java/com/udemy/springcourse/App.java
+++ src/main/java/com/udemy/springcourse/App.java
@@ -17,7 +17,7 @@ public class App {
         try (sessionFactory) {
             session.beginTransaction();
 
-            List<Person> people = session.createQuery("FROM Person WHERE age > 30").getResultList();
+            List<Person> people = session.createQuery("FROM Person WHERE name LIKE 'T%'").getResultList();
             for(Person person: people) {
                 System.out.println(person);
             }
