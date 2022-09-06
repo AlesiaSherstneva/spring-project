fdb3c8bb2c6b12aa7942de4c05fd83e7059a4725
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-09-07 00:26:35 +0300

    Lesson 52 - Get one person and his items

--- src/main/java/com/udemy/springcourse/App.java
+++ src/main/java/com/udemy/springcourse/App.java
@@ -1,12 +1,18 @@
 package com.udemy.springcourse;
 
+import com.udemy.springcourse.pojo.Item;
+import com.udemy.springcourse.pojo.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
@@ -14,7 +20,11 @@ public class App {
         try {
             session.beginTransaction();
 
+            Person person = session.get(Person.class, 3);
+            System.out.println(person);
 
+            List<Item> items = person.getItems();
+            System.out.println(items);
 
             session.getTransaction().commit();
         } finally {
