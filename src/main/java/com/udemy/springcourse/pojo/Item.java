fa97dada64fe8fccf0c0029c1a9b9ebc3648a544
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-09-06 23:11:21 +0300

    Lesson 52 - Add pojos

---
+++ src/main/java/com/udemy/springcourse/pojo/Item.java
@@ -0,0 +1,42 @@
+package com.udemy.springcourse.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Item")
+public class Item {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    public Item() {
+    }
+
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/udemy/springcourse/pojo/Person.java
@@ -0,0 +1,55 @@
+package com.udemy.springcourse.pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
