ea14c911c119bee2a416ee7c441de9b87afdb1ef
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-09-17 07:41:37 +0300

    Lesson 55 - Refactor passport's class for dividing primary and foreign keys

--- src/main/java/com/udemy/springcourse/pojo/Passport.java
+++ src/main/java/com/udemy/springcourse/pojo/Passport.java
@@ -5,14 +5,18 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "Passport")
-public class Passport implements Serializable {
+public class Passport {
     @Id
-    @OneToOne
-    @JoinColumn(name = "person_id", referencedColumnName = "id")
-    private Person person;
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     @Column(name = "passport_number")
     private int passportNumber;
+
+    @OneToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person person;
 
     public Passport() {
     }
@@ -20,6 +24,14 @@ public class Passport implements Serializable {
     public Passport(Person person, int passportNumber) {
         this.person = person;
         this.passportNumber = passportNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Person getPerson() {
