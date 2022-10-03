bd69804734cf1549f3c0fcf304c54fdf4277a55f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-04 00:56:18 +0300

    Lesson 62 - Add new(old) item class

---
+++ src/main/java/com/udemy/springcourse/pojo/Item.java
@@ -0,0 +1,57 @@
+package com.udemy.springcourse.pojo;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "Item")
+public class Item {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Item name should not be empty")
+    @Column(name = "item_name")
+    private String itemName;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Person owner) {
+        this.itemName = itemName;
+        this.owner = owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", itemName='" + itemName + '\'' +
+                '}';
+    }
+}
