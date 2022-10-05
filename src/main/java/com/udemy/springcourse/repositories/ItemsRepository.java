a34089188667ca9f9088493dd1f7aac8223d3bd4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-05 18:49:22 +0300

    Lesson 62 - Change items' repository to right syntax

--- src/main/java/com/udemy/springcourse/repositories/ItemsRepository.java
+++ src/main/java/com/udemy/springcourse/repositories/ItemsRepository.java
@@ -2,12 +2,13 @@ package com.udemy.springcourse.repositories;
 
 import com.udemy.springcourse.pojo.Item;
 import com.udemy.springcourse.pojo.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ItemsRepository {
+public interface ItemsRepository extends JpaRepository<Item, Integer> {
     List<Item> findByItemName(String itemName);
 
     List<Item> findByOwner(Person owner);
