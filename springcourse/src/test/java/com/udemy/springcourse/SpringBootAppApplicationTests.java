fcccd4d7bdd13903de621a7edfcc30bd31bcf09a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-16 13:17:50 +0300

    Lesson 71 - Create Spring Boot App

---
+++ springcourse/src/test/java/com/udemy/springcourse/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
