83d6705c6ca3bb6c54eb1e09bc30e57dc8728497
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-21 13:59:33 +0300

    Update main mapping, rename jar-file

--- project2Boot/src/main/java/com/udemy/springcourse/project2Boot/controller/HomeController.java
+++ project2Boot/src/main/java/com/udemy/springcourse/project2Boot/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/library")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping()
     public String homePage() {
         return "index";
     }
