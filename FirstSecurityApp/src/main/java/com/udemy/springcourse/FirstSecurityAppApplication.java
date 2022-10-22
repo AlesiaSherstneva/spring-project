76d0d31a0e46c603ed90ceb6a1194672802919a5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-22 19:21:49 +0300

    Lesson 77 - Add first Spring Security project

---
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/FirstSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSecurityAppApplication.class, args);
+	}
+
+}
