afa7f6b5d22c8ce7ac71c8a32e12cb2a3f2deb7b
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-22 20:02:04 +0300

    Lesson 77 - Add properties and delete unused import from controller

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/HelloController.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/HelloController.java
@@ -2,7 +2,6 @@ package com.udemy.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
