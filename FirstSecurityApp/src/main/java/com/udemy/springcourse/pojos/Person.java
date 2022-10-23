62c9ec3b2d0ff0b445db3d7a673a7b48e98eebb4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-23 15:21:26 +0300

    Lesson 78 - Add entity

---
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/pojos/Person.java
@@ -0,0 +1,36 @@
+package com.udemy.springcourse.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Person")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
+    @Column(name = "username")
+    private String username;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+}
