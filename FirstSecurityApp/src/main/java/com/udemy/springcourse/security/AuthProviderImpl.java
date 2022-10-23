6a13afd9e84f43dff796ae2ec11e50a607a0c830
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-23 16:33:10 +0300

    Lesson 78 - Add authentication provider

---
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/security/AuthProviderImpl.java
@@ -0,0 +1,35 @@
+package com.udemy.springcourse.security;
+
+import com.udemy.springcourse.services.PersonDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+@AllArgsConstructor
+public class AuthProviderImpl implements AuthenticationProvider {
+    private final PersonDetailsService personDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        UserDetails personDetails = personDetailsService.loadUserByUsername(username);
+        String password = authentication.getCredentials().toString();
+        if (!password.equals(personDetails.getPassword())) {
+            throw new BadCredentialsException("Incorrect password");
+        }
+        return new UsernamePasswordAuthenticationToken(personDetails, password, Collections.emptyList());
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return true;
+    }
+}
