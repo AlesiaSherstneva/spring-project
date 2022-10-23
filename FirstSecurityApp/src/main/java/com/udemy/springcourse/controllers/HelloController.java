7067148ee8ff782b082e71a130a611614da1a170
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-23 17:29:16 +0300

    Lesson 78 - Add method showing person

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/HelloController.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/HelloController.java
@@ -1,5 +1,8 @@
 package com.udemy.springcourse.controllers;
 
+import com.udemy.springcourse.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
         return "hello";
     }
 }
