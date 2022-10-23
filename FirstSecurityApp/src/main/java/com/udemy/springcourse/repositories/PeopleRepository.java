b33bbf02446893e56436ba14bb990111f32419c0
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-23 16:00:14 +0300

    Lesson 78 - Add people repository

---
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/repositories/PeopleRepository.java
@@ -0,0 +1,12 @@
+package com.udemy.springcourse.repositories;
+
+import com.udemy.springcourse.pojos.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
