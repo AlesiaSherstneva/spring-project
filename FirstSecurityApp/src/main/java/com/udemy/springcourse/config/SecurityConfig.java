c79de4e3a8bdecbabb1d83807ccec07a287c0f7b
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-24 17:49:27 +0300

    Lesson 79 - Update config to not using authproviderimpl

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.udemy.springcourse.config;
 
-import com.udemy.springcourse.security.AuthProviderImpl;
+import com.udemy.springcourse.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
