b0836608938a80467884b53f8f6a60303e3b7cfe
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-25 15:41:39 +0300

    Lesson 81 - Add registration method

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/AuthController.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.udemy.springcourse.controllers;
 
+import com.udemy.springcourse.pojos.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +12,10 @@ public class AuthController {
     @GetMapping("/login")
     public String loginPage() {
         return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("person") Person person) {
+        return "auth/registration";
     }
 }
