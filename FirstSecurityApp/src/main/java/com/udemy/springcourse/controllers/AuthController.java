aee1711ff806db71e870357f1a1c5ed464b7dfd8
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-25 16:17:41 +0300

    Lesson 81 - Add registration functionality to controller

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/AuthController.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/AuthController.java
@@ -1,14 +1,30 @@
 package com.udemy.springcourse.controllers;
 
 import com.udemy.springcourse.pojos.Person;
+import com.udemy.springcourse.services.RegistrationService;
+import com.udemy.springcourse.util.PersonValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    private final PersonValidator personValidator;
+    private final RegistrationService registrationService;
+
+    @Autowired
+    public AuthController(PersonValidator personValidator, RegistrationService registrationService) {
+        this.personValidator = personValidator;
+        this.registrationService = registrationService;
+    }
+
     @GetMapping("/login")
     public String loginPage() {
         return "auth/login";
@@ -17,5 +33,16 @@ public class AuthController {
     @GetMapping("/registration")
     public String registrationPage(@ModelAttribute("person") Person person) {
         return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("person") @Valid Person person,
+                                      BindingResult bindingResult) {
+        personValidator.validate(person, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "/auth/registration";
+        }
+        registrationService.register(person);
+        return "redirect:/auth/login";
     }
 }
