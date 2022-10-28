a31b519e95877cac51b3a26ea20f10bac7d1102b
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-28 23:03:24 +0300

    Lesson 82 - Add logout configuration

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
@@ -29,7 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/hello", true)
-                .failureUrl("/auth/login?error");
+                .failureUrl("/auth/login?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
     }
 
     @Override
