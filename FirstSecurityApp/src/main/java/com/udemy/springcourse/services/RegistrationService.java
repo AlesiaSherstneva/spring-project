59628725945cd9ee5537e0785197cfbd224dbc57
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-10-29 14:37:26 +0300

    Lesson 83 - Add password encoder

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/services/RegistrationService.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/services/RegistrationService.java
@@ -3,20 +3,24 @@ package com.udemy.springcourse.services;
 import com.udemy.springcourse.pojos.Person;
 import com.udemy.springcourse.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
