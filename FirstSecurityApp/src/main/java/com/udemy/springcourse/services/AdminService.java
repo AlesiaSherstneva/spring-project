426770597a046c50cd53d2c5a0f898ad0d9277fb
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-01 16:07:17 +0300

    Lesson 86 - Add @PreAuthorize annotation

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/services/AdminService.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/services/AdminService.java
@@ -1,9 +1,11 @@
 package com.udemy.springcourse.services;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void doAdminStuff() {
         System.out.println("Only admin here");
     }
