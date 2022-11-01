defe59d9019cc4ca080b6eb4c027db88da941231
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-01 15:57:58 +0300

    Lesson 86 - Delete admin authorities from configuration and add method security

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.udemy.springcourse.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final PersonDetailsService personDetailsService;
 
@@ -22,7 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "error").permitAll()
                 .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
