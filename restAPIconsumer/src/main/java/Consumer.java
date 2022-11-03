f7ec7c077471784f111f3d63d61b8475b55b2db3
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-03 13:59:26 +0300

    Lesson 89 - Make POST-request in foreign site

--- restAPIconsumer/src/main/java/Consumer.java
+++ restAPIconsumer/src/main/java/Consumer.java
@@ -1,11 +1,21 @@
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Consumer {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "https://reqres.in/api/users/2";
-        String response = restTemplate.getForObject(url, String.class);
+        Map<String, String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name", "Test name");
+        jsonToSend.put("job", "Test job");
+
+        HttpEntity <Map<String, String>> request = new HttpEntity<>(jsonToSend);
+
+        String url = "https://reqres.in/api/users/";
+        String response = restTemplate.postForObject(url, request, String.class);
 
         System.out.println(response);
     }
