80ebb6140bc041aedc32717d8efdc0e86dae0182
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-04 15:58:41 +0300

    Lesson 90 - Change @ResponseBody to @RestController

--- firstRestApp/src/main/java/com/udemy/springcourse/controllers/FirstRESTController.java
+++ firstRestApp/src/main/java/com/udemy/springcourse/controllers/FirstRESTController.java
@@ -1,11 +1,11 @@
 package com.udemy.springcourse.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class FirstRESTController {
     @ResponseBody
