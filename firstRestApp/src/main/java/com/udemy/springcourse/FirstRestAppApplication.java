894c5f2f51c066bbd1703b0d838cce4743a3758e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-04 15:24:32 +0300

    Lesson 90 - Generate new REST application and application properties

---
+++ firstRestApp/src/main/java/com/udemy/springcourse/FirstRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstRestAppApplication.class, args);
+    }
+
+}
