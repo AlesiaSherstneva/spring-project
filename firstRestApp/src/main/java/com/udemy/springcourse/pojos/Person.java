80c3b7db47c471568bfd2c788b181be1b5eff12e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-06 21:13:58 +0300

    Lesson 92 - Add person's validation

--- firstRestApp/src/main/java/com/udemy/springcourse/pojos/Person.java
+++ firstRestApp/src/main/java/com/udemy/springcourse/pojos/Person.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "person")
@@ -19,12 +23,17 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between two and thirty characters")
     @Column(name = "name")
     private String name;
 
+    @Min(value = 0, message = "Age should be greater than 0")
     @Column(name = "age")
     private int age;
 
+    @Email
+    @NotEmpty(message = "Email should not be empty")
     @Column(name = "email")
     private String email;
 }
