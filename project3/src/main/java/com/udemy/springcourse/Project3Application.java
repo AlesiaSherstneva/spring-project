313e738099175264fdf6005e3508402c0a9b5b66
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-08 22:57:58 +0300

    Create project3

---
+++ project3/src/main/java/com/udemy/springcourse/Project3Application.java
@@ -0,0 +1,13 @@
+package com.udemy.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project3Application.class, args);
+    }
+
+}
---
+++ project3/src/test/java/com/udemy/springcourse/Project3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
