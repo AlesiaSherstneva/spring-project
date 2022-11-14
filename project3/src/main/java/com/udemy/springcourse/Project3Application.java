93a7ce57e15c1ab04aee00145bb9431a5435c9b9
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-14 22:09:59 +0300

    Create modelmapper's bean

--- project3/src/main/java/com/udemy/springcourse/Project3Application.java
+++ project3/src/main/java/com/udemy/springcourse/Project3Application.java
@@ -1,7 +1,9 @@
 package com.udemy.springcourse;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Project3Application {
@@ -10,4 +12,8 @@ public class Project3Application {
         SpringApplication.run(Project3Application.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
