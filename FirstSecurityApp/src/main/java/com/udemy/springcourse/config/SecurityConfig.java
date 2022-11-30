866fe8fbd512b67bbee3aaf56199c9922a46c427
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-30 23:06:17 +0300

    Lesson 95 - Create performLogin() method for getting new token if old is expired

--- FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.udemy.springcourse.config;
 import com.udemy.springcourse.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -55,5 +56,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/AuthController.java
+++ FirstSecurityApp/src/main/java/com/udemy/springcourse/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.udemy.springcourse.controllers;
 
+import com.udemy.springcourse.dto.AuthenticationDTO;
 import com.udemy.springcourse.dto.PersonDTO;
 import com.udemy.springcourse.pojos.Person;
 import com.udemy.springcourse.security.JWTUtil;
@@ -7,6 +8,9 @@ import com.udemy.springcourse.services.RegistrationService;
 import com.udemy.springcourse.util.PersonValidator;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +24,15 @@ public class AuthController {
     private final RegistrationService registrationService;
     private final JWTUtil jwtUtil;
     private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController(PersonValidator personValidator, RegistrationService registrationService, JWTUtil jwtUtil, ModelMapper modelMapper) {
+    public AuthController(PersonValidator personValidator, RegistrationService registrationService, JWTUtil jwtUtil, ModelMapper modelMapper, AuthenticationManager authenticationManager) {
         this.personValidator = personValidator;
         this.registrationService = registrationService;
         this.jwtUtil = jwtUtil;
         this.modelMapper = modelMapper;
+        this.authenticationManager = authenticationManager;
     }
 
 /*    @GetMapping("/login")
@@ -41,7 +47,7 @@ public class AuthController {
 
     @PostMapping("/registration")
     public Map<String, String> performRegistration(@RequestBody @Valid PersonDTO personDTO,
-                                      BindingResult bindingResult) {
+                                                   BindingResult bindingResult) {
         Person person = convertToPerson(personDTO);
         personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors()) {
@@ -49,6 +55,19 @@ public class AuthController {
         }
         registrationService.register(person);
         String token = jwtUtil.generateToken(person.getUsername());
+        return Map.of("jwt-token", token);
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> performLogin(@RequestBody AuthenticationDTO authDTO) {
+        UsernamePasswordAuthenticationToken authInputToken =
+                new UsernamePasswordAuthenticationToken(authDTO.getUsername(), authDTO.getPassword());
+        try {
+            authenticationManager.authenticate(authInputToken);
+        } catch (BadCredentialsException exception) {
+            return Map.of("message", "Incorrect credentials!");
+        }
+        String token = jwtUtil.generateToken(authDTO.getUsername());
         return Map.of("jwt-token", token);
     }
 
